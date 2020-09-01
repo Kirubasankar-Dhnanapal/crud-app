7bb2af575b2ad0c8c8012e8e77fafe6332b0abc0
kirubasankar <45040028+Kirubasankar-Dhnanapal@users.noreply.github.com>
2020-09-01 12:30:16 +0530

    lazyload sample

--- src/Routers.js
+++ src/Routers.js
@@ -1,4 +1,4 @@
-import { Route, Switch,HashRouter } from "react-router-dom";
+import { Route, Switch,BrowserRouter } from "react-router-dom";
 import React, { Component } from "react";
 import App from './App';
 import Lazyload from './lazyload';
@@ -14,12 +14,12 @@ class Routers extends Component {
 
     render() {
         return (
-            <HashRouter>
+            <BrowserRouter>
                 <Switch>
                     <Route exact path='/' component={App}/>
                     <Route path='/lazy' component={Lazyload}/>
                 </Switch>
-            </HashRouter>
+            </BrowserRouter>
         );
     }
 }
